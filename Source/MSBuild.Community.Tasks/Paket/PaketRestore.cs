c830e3aacfe10d24c8fb343c0e732515e66b7bc9
Andre Kwakernaak <andre.kwakernaak@gmail.com>
2016-06-27 22:56:34 +0200

    Added PaketRestore to support Pakets restore functionality

---
+++ Source/MSBuild.Community.Tasks/Paket/PaketRestore.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using Microsoft.Build.Utilities;
+
+namespace MSBuild.Community.Tasks.Paket
+{
+    public class PaketRestore : ToolTask
+    {
+        /// <summary>
+        /// If true Paket will run in verbose mode and show detailed information.
+        /// </summary>
+        public bool Verbose { get; set; }
+
+        /// <summary>
+        /// Touches project files referencing packages which are being restored, to help incremental 
+        /// build tools detecting the change.
+        /// </summary>
+        public bool TouchAffectedRefs { get; set; }
+
+        protected override string ToolName => "paket.exe";
+
+        protected override string GenerateCommandLineCommands()
+        {
+            var builder = new CommandLineBuilder();
+            builder.AppendSwitch("restore");
+            if (Verbose)
+            {
+                builder.AppendSwitch("-v");
+            }
+
+            if (TouchAffectedRefs)
+            {
+                builder.AppendSwitch("--touch-affected-refs");
+            }
+            return builder.ToString();
+        }
+
+        protected override string GenerateFullPathToTool()
+        {
+            return string.IsNullOrWhiteSpace(ToolPath)
+                ? Path.Combine(".paket", ToolName)
+                : Path.Combine(ToolPath, ToolName);
+        }
+    }
+}
